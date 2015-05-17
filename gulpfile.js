f5a8ba0031c843f100c175b8740fe48decd31bff
Joyee Cheung <joyeec9h3@gmail.com>
2015-05-17 22:44:24 +0800

    start working

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,17 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
-var autoprefixer = require('autoprefixer-core');
-var mqpacker = require('css-mqpacker');
-var csswring = require('csswring');
-
 var path = require('path');
 var http = require('http');
 var st = require('st');
+var moment = require('moment');
 
 // PostCSS
 gulp.task('css', function () {
   var processors = [
-    autoprefixer({browsers: ['last 1 version']}),
-    mqpacker,
-    csswring
+    require('autoprefixer-core')({browsers: ['last 1 version']}),
+    require('css-mqpacker'),
+    require('csswring')
   ];
 
   return gulp.src('./src/css/**/*.css')
@@ -35,21 +32,42 @@ gulp.task('markup', function() {
         return require('highlight.js').highlightAuto(code).value;
       }
     }))
-    // .pipe(plugins.layout({
-    //   layout: './src/jade/layout/diary-layout.jade'
-    // }))
+    .pipe(plugins.layout(function(file) {
+      var name = path.parse(file.path).name;
+      var date = moment(new Date(name));
+      
+      if (!date.isValid()) {
+        return {
+          layout: './src/jade/layout/plain-layout.jade',
+          data_lang: 'en',
+          title: name
+        }
+      } else if (name.length > 7) {   // full date
+        return {
+          layout: './src/jade/layout/diary-layout.jade',
+          data_lang: 'en',
+          title: date.format('MMMM D, YYYY')
+        }
+      } else {  // month summary
+        return {
+          layout: './src/jade/layout/month-layout.jade',
+          data_lang: 'en',
+          title: date.format('MMMM, YYYY')
+        }
+      }
+    }))
     .pipe(gulp.dest('./dist/'))
-    // .pipe(plugins.livereload());
+    .pipe(plugins.livereload());
 });
 
 // watch
 gulp.task('watch', ['server'], function() {
   plugins.livereload.listen({ basePath: 'dist' });
-  gulp.watch(['./my-tech-diary/**/*.md', './src/jade/**/*.jade'], ['markup']);
+  gulp.watch(['./diary/**/*.md', './src/jade/**/*.jade'], ['markup']);
   gulp.watch('./src/css/**/*.css', ['css']);
 });
 
-gulp.task('build', ['diary', 'css']);
+gulp.task('build', ['markup', 'css']);
 
 // Launch server
 gulp.task('server', ['build'], function(done) {
