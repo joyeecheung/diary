1c0329655e972814378abf845207e13e008b6a73
threegoldstone <hxljustdoit@gmail.com>
2015-07-03 13:27:58 +0800

    config

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
   author: {
-    name: "Joyee Cheung",
-    url: "https://github.com/joyeecheung"
+    name: "Chris Han",
+    url: "https://github.com/threegoldstone"
   },
   repo: {  // for the markdown source
     content: "https://github.com/joyeecheung/my-tech-diary",
     as_submodule: 'diary'
   },
   base_url: '/diary',  // remote directory for the website
-  ga_id: 'UA-63818301-1',  // Optional: Universal Google Analytics ID
+  ga_id: 'UA-64731433-1',
   title: 'My Tech Diary'
-}
+};
